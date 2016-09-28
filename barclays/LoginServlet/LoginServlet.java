44d293aab83bf1ecc272bb17dfc4bdc0d96efbe5
karthikreddy31 <mum.karthik@gmail.com>
2016-09-28 18:45:49 -0400

    Add files via upload

---
+++ barclays/LoginServlet/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.karthik.ls;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(description = "Simple Login Servlet", urlPatterns = { "/LoginServlet" })
+public class LoginServlet extends HttpServlet {
+	
+	Random random = new Random();
+	private static final long serialVersionUID = 1L;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String USERNAME = request.getParameter("username");
+		String PASSWORD = request.getParameter("passwd");
+		request.getSession(true).setAttribute("token", random);
+		response.getWriter().println("Hello Webpage");
+		doGet(request, response);
+	}
+
+}
